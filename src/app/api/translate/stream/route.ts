e867c761c047050780636785ac233645e2f12668
Yinan Zhou <yinan.a.zhou@gmail.com>
2026-04-14 15:52:30 -0400

    style: format frontend & update ignore

--- src/app/api/translate/stream/route.ts
+++ src/app/api/translate/stream/route.ts
@@ -38,7 +38,7 @@ export async function POST(req: NextRequest) {
     headers: {
       "Content-Type": "text/event-stream",
       "Cache-Control": "no-cache",
-      "Connection": "keep-alive",
+      Connection: "keep-alive",
       "X-Accel-Buffering": "no",
     },
   });
